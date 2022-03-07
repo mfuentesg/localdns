5905e4a7672cfde83883bc9e07cb6356674e8081
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2022-03-06 22:09:57 -0300

    simplify and add testing for dns package

--- server/dns/dns.go
+++ server/dns/dns.go
@@ -1,127 +1,42 @@
 package dns
 
 import (
-	"errors"
-	"log"
-	"net"
-
-	"github.com/mfuentesg/localdns/storage"
 	"github.com/miekg/dns"
 )
 
-type handler struct {
-	st        storage.Storage
-	addr      string
-	protocol  string
-	dnsServer string
+type Server struct {
+	Addr     string
+	Protocol string
+	server   *dns.Server
 }
 
-type Option func(*handler)
-
-func WithStorage(st storage.Storage) Option {
-	return func(s *handler) {
-		s.st = st
-	}
-}
+type Option func(*Server)
 
 func WithAddr(addr string) Option {
-	return func(h *handler) {
-		h.addr = addr
+	return func(s *Server) {
+		s.Addr = addr
 	}
 }
 
 func WithProtocol(protocol string) Option {
-	return func(h *handler) {
-		h.protocol = protocol
+	return func(s *Server) {
+		s.Protocol = protocol
 	}
 }
 
-func WithDNSServer(dnsServer string) Option {
-	return func(h *handler) {
-		h.dnsServer = dnsServer
-	}
-}
-
-func (h *handler) forwardQuery(message *dns.Msg) (*dns.Msg, error) {
-	c := &dns.Client{Net: "udp"}
-	conn, err := c.Dial(h.dnsServer)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// TODO: should dial connection be closed?
-	// defer func() { _ = conn.Close() }()
-
-	response, _, err := c.ExchangeWithConn(message, conn)
-	if err != nil {
-		return nil, err
-	}
-
-	return response, nil
-}
-
-func (h *handler) buildMessage(m *dns.Msg) (*dns.Msg, error) {
-	question := m.Question[0]
-
-	if question.Qtype != dns.TypeA {
-		return nil, errors.New("unsupported dns type")
-	}
-
-	domain := question.Name
-	record, err := h.st.Get(domain)
-
-	if errors.Is(err, storage.ErrRecordNotFound) {
-		log.Printf("%s: domain '%s' not found\n", h.protocol, domain)
-		return h.forwardQuery(m)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	var message dns.Msg
-	message.SetReply(m)
-	message.Authoritative = true
-	message.Answer = append(message.Answer, &dns.A{
-		Hdr: dns.RR_Header{
-			Name:   domain,
-			Rrtype: dns.TypeA,
-			Class:  dns.ClassINET,
-			Ttl:    60,
-		},
-		A: net.ParseIP(record.IP),
-	})
-
-	return &message, nil
-}
-
-func (h *handler) ServeDNS(w dns.ResponseWriter, m *dns.Msg) {
-	message, err := h.buildMessage(m)
-
-	if err != nil {
-		log.Printf("could not build message, reason: %+v\n", err)
-		_ = w.WriteMsg(m)
-		return
-	}
-
-	_ = w.WriteMsg(message)
-}
-
-func New(opts ...Option) dns.Server {
-	h := handler{
-		addr:      ":53",
-		protocol:  "udp",
-		dnsServer: "8.8.8.8:53",
+func New(handler dns.Handler, opts ...Option) *dns.Server {
+	srv := Server{
+		Addr:     ":53",
+		Protocol: "udp",
 	}
 
 	for _, opt := range opts {
-		opt(&h)
+		opt(&srv)
 	}
 
-	return dns.Server{
-		Addr:    h.addr,
-		Net:     h.protocol,
-		Handler: &h,
+	return &dns.Server{
+		Addr:    srv.Addr,
+		Net:     srv.Protocol,
+		Handler: handler,
 	}
 }
--- server/dns/dns_test.go
+++ server/dns/dns_test.go
@@ -1,24 +1,36 @@
 package dns
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/miekg/dns"
+	"github.com/stretchr/testify/assert"
+)
+
+type fakeHandler struct{}
+
+func (h *fakeHandler) ServeDNS(w dns.ResponseWriter, m *dns.Msg) {
+	_ = w.WriteMsg(m)
+}
 
 func TestNew(t *testing.T) {
-}
+	server := New(new(fakeHandler))
 
-func TestHandler_ServeDNS(t *testing.T) {
-}
-
-func TestWithDNSServer(t *testing.T) {
+	assert.IsType(t, new(dns.Server), server)
+	assert.Equal(t, server.Net, "udp")
+	assert.Equal(t, server.Addr, ":53")
 }
 
 func TestWithAddr(t *testing.T) {
+	addr := "0.0.0.0:8053"
+	server := New(new(fakeHandler), WithAddr(addr))
+
+	assert.Equal(t, server.Addr, addr)
 }
 
 func TestWithProtocol(t *testing.T) {
-}
+	protocol := "tcp"
+	server := New(new(fakeHandler), WithProtocol(protocol))
 
-func TestWithStorage(t *testing.T) {
-}
-
-func BenchmarkHandler_ServeDNS(b *testing.B) {
+	assert.Equal(t, server.Net, protocol)
 }
